318fbe93e11d2af77be59423cdf4b837b0edb6fd
jordanmosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2021-11-13 12:56:13 -0800

    example HTTP request function

--- backend/index.ts
+++ backend/index.ts
@@ -1,12 +1,18 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+const axios = require('axios');
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.get("/http", async (req,res) => {
+  const response = await axios('https://api.sampleapis.com/baseball/hitsSingleSeason/');
+  const data = response.data;
+  //Here you would probably send send your data off to another function.
+  res.send(data);
 })
-
-
 
 
 
@@ -15,3 +21,10 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+
+interface FrontendRequest{
+  address: string,
+  timeRange: 1 | 3 | 6 | 12,
+  radius: 15 | 25 | 50 | 100,
+}
