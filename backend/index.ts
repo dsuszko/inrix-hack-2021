5b761451bd6fffb9c80d58fe4420908cab829272
jordanmosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2021-11-13 16:46:39 -0800

    fixed trips api request

--- backend/index.ts
+++ backend/index.ts
@@ -20,12 +20,11 @@ app.get("/http", async (req,res) => {
 });
 
 app.get("/test", async (req,res) => {
-  const response = await tradeAreaTrips("37.734|-122.47", ">=2021-11-13T00:00");
+  const response = await tradeAreaTrips("37.734%7C-122.47", "%3E%3D2020-12-01T00%3A00");
   // const response = await fetchToken();
-  const data = response.data;
-  console.log(response.data);
+  console.log(response);
   //Here you would probably send send your data off to another function.
-  res.send(data);
+  res.send(response);
 });
 
 
@@ -35,6 +34,73 @@ app.get("/test", async (req,res) => {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+async function fetchToken() {
+  const axiosConfig = {
+    headers: {
+      "content-type": "application/json",
+      Accept: "application/json",
+    },
+  };
+
+  const tokenRes = await axios
+    .get("https://api.iq.inrix.com/auth/v1/appToken?appId="+process.env.APP_ID+"&hashToken="+process.env.HASH, axiosConfig)
+    .then((response) => {
+      // if (response.status === 200) {
+      // this.accessToken = response.data;
+      console.log(response.data);
+      return "Bearer "+response.data.result.token;
+      // }
+    })
+    .catch((error) => {
+      console.error("An error occured: ", error);
+      // return error.message;
+      throw "error";
+    });
+  return tokenRes;
+}
+
+async function tradeAreaTrips(point, startDate) {
+  const axiosConfig = {
+    headers: {
+      "content-type": "application/json",
+      "Accept": "application/json",
+      "Authorization": await fetchToken(),
+    }
+  }
+
+  let od: string = "destination";
+  let geoFilterType: string = "circle";
+  let radius: string = "200ft";
+  //point is given
+  let limit: number = 100;
+  let provider: string = "consumer";
+  //start date is given
+  let endDate: string = "%3C%3D2020-12-31T00%3A00";
+  let endPointType: number = 3;
+  //tripLength is given
+
+  const tripsResponse = await axios
+      .get("https://api.iq.inrix.com/v1/trips?"+
+      "od="+od+"&geoFilterType="+geoFilterType+
+      "&radius="+radius+"&points="+point+
+      "&limit="+limit.toString()+"&startDateTime="+startDate+"&endDateTime="+endDate+"&endpointType=3",axiosConfig)
+      // .get("https://api.iq.inrix.com/v1/trips?od="+od+"&geoFilterType="+geoFilterType+"&points="+point+"&limit="+limit
+      // +"&providerType="+provider+"&startDateTime="+startDate+"&endDateTime="+endDate+"&endpointType=3", axiosConfig)
+      .then((response) => {
+          return response.data;
+      })
+      .catch((error) => {
+          console.error("An error occured: ", error);
+          throw "error";
+      });
+  
+  return tripsResponse;
+}
+
+
+
+
 
 
 interface FrontendRequest{
@@ -64,63 +130,4 @@ interface BoxData{
   startDate: Date;
   endDate: Date;
   numberOfVisits: number;
-}
-
-async function fetchToken() {
-  const axiosConfig = {
-    headers: {
-      "content-type": "application/json",
-      Accept: "application/json",
-    },
-  };
-
-  const tokenRes = await axios
-    .get("https://api.iq.inrix.com/auth/v1/appToken?appId="+process.env.APP_ID+"&hashToken="+process.env.HASH, axiosConfig)
-    .then((response) => {
-      // if (response.status === 200) {
-      // this.accessToken = response.data;
-      console.log(response.data);
-      return response.data;
-      // }
-    })
-    .catch((error) => {
-      console.error("An error occured: ", error);
-      // return error.message;
-      throw "error";
-    });
-  return tokenRes;
-}
-
-async function tradeAreaTrips(point, startDate) {
-  const axiosConfig = {
-      "content-type": "application/json",
-      Accept: "application/json",
-      Authorization: await fetchToken(),
-  }
-
-  let od: string = "destination";
-  let geoFilterType: string = "circle";
-  let radius: string = "200ft";
-  //point is given
-  let limit: number = 100;
-  let provider: string = "consumer";
-  //start date is given
-  let endDate: string = "<=2021-11-13T15:00";
-  let endPointType: number = 3;
-  //tripLength is given
-
-  const tripsResponse = await axios
-      .get("https://api.iq.inrix.com/v1/trips?od="+od+"&geoFilterType="+geoFilterType+"&points="+point+"&limit="+limit
-      +"&providerType="+provider+"&startDateTime="+startDate+"&endDateTime="+endDate+"&endpointType=3", axiosConfig)
-      // .get("https://trade-areas-api.inrix.com/v1/trips?od="+od+"&geoFilterType="+geoFilterType+"&points="+point+"&limit="+limit
-      //  +"&providerType="+provider+"&startDateTime="+startDate+"&endDateTime="+endDate+"&endpointType=3", axiosConfig)
-      .then((response) => {
-          return response.data;
-      })
-      .catch((error) => {
-          console.error("An error occured: ", error);
-          throw "error";
-      });
-  
-  return tripsResponse;
 }
