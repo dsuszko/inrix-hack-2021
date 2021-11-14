6efa942c07ec6a716c108f553ebc9a81d5055c57
jordanmosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2021-11-14 03:06:40 -0800

    2 maps

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -11,8 +11,9 @@ import * as leaflet from 'leaflet';
 export class HomeComponent implements AfterViewInit {
 
   displayOptions: DisplayOption[] = [
-    {value: false, display: "Raw data"},
-    {value: true, display: "Change"},
+    {value: 0, display: "Raw data"},
+    {value: 1, display: "Compare (diff)"},
+    {value: 2, display: "Compare (split)"},
   ];
 
   selectedRadius: number = 5;
@@ -40,7 +41,7 @@ export class HomeComponent implements AfterViewInit {
   endDate2: Date | undefined = new Date(2020,11,31);
 
   constructor(private http: HttpClient) {
-    this.showComparion = false;
+    this.showComparison = 0;
   }
 
   async searchOSMApi(){
@@ -49,22 +50,27 @@ export class HomeComponent implements AfterViewInit {
   }
 
   points: any[] = [];
+  points2: any[] = [];
 
   updateMap(){
+    setTimeout(() => this.map.invalidateSize(),100);
     if(this.endDate==null){
       console.log("Not updating map");
       return;
     }
-    console.log(this.showComparion);
-    if(this.showComparion){
+    console.log(this.showComparison);
+    if(this.showComparison==1){
       this.comparisonMap();
     }
     else{
       this.rawDataMap();
+      if(this.showComparison==2){
+        this.rawDataMap2();
+      }
     }
   }
 
-  showComparion: boolean = false;
+  showComparison: number = 0;
 
   async getDateData(lat:string,lon:string){
     const radius = this.selectedRadius;
@@ -73,11 +79,14 @@ export class HomeComponent implements AfterViewInit {
       "http://localhost:3000/getTripsByDate?lat="+lat+"&lon="+lon
       +"&radius="+radius.toString()+"&boxSize="+boxSize.toString()
       ).toPromise() ?? [];
-    if(this.showComparion){
+    if(this.showComparison==1){
       this.comparisonMap();
     }
     else{
       this.rawDataMap();
+      if(this.showComparison==2){
+        this.rawDataMap2();
+      }
     }
   }
 
@@ -213,14 +222,64 @@ export class HomeComponent implements AfterViewInit {
     var myIcon = leaflet.icon({
       iconUrl: 'assets/White_Logo.png',
       iconSize: [32,32],
-
     });
-    const marker = leaflet.marker([this.selectedAddress?.lat ?? 0, this.selectedAddress?.lon ?? 0], {icon: myIcon}).addTo(this.map);
+    const marker = leaflet.marker([this.selectedAddress?.lat ?? 0, this.selectedAddress?.lon ?? 0], {icon: myIcon});
     this.points.push(marker);
     marker.addTo(this.map);
-    // const circle = leaflet.circle([this.selectedAddress?.lat ?? 0, this.selectedAddress?.lon ?? 0], {color: "#ffffff", radius: 152.4, fillOpacity: 1});
-    // this.points.push(circle);
-    // circle.addTo(this.map);
+  }
+
+  rawDataMap2(){
+    for(let p of this.points2){
+      this.map2.removeLayer(p);
+    }
+    let start = this.startDate2.getDate()-1;
+    let end = this.endDate2?.getDate() ?? 31;
+    this.points2 = [];
+    for(let row of (this.dateData ?? [])){
+      for(let box of row){
+        let count = box.data.slice(start,end).map(e => e.numberOfVisits).reduce((a, b) => a + b);
+        let color;
+        let colors = [
+          '#1fff9e',//#1bf1ea
+          '#0abeff',//#1b8fee
+          '#000eff',//#2b1bf2
+          '#a000ff',//#9d15ec
+          '#ff1f69',//#f518d8
+        ];
+        if(count==0){
+          continue;
+        } else if(count==1){
+          color = colors[0]
+        }
+        else if(count<=3){
+          color = colors[1]
+        }
+        else if(count<=6){
+          color=colors[2]
+        }
+        else if(count<=10){
+          color=colors[3]
+        }
+        else{
+          color=colors[4]
+        }
+        let rect = leaflet.rectangle([box.p1,box.p2], {color: color, weight: 1, stroke: false, fillOpacity: 0.4})
+        .bindTooltip(count.toString()+" people visited");
+        rect.on("click",() => {
+          console.log(box);
+        });
+        this.points2.push(rect);
+        rect.addTo(this.map2);
+      }
+    }
+    var myIcon = leaflet.icon({
+      iconUrl: 'assets/White_Logo.png',
+      iconSize: [32,32],
+
+    });
+    const marker = leaflet.marker([this.selectedAddress?.lat ?? 0, this.selectedAddress?.lon ?? 0], {icon: myIcon});
+    this.points2.push(marker);
+    marker.addTo(this.map2);
   }
 
   selectAddress(res: OSMPlace | undefined){
@@ -243,6 +302,7 @@ export class HomeComponent implements AfterViewInit {
   }
 
   map: any;
+  map2: any;
 
   boxZoom: number = 0.5;
 
@@ -251,24 +311,33 @@ export class HomeComponent implements AfterViewInit {
       center: [ 37.76, -122.435 ],
       zoom: 13
     });
+    this.map2 = leaflet.map('map2', {
+      center: [ 37.76, -122.435 ],
+      zoom: 13
+    });
     this.map.on('zoomend', (e:any) => {
-      // c
+      let center = this.map.getCenter();
       let newZoom = e.target._zoom;
-      if(this.boxZoom == 0.5 && newZoom >=16){
-        this.boxZoom = 0.25;
-        if(this.selectedAddress!=null){
-          this.selectAddress(this.selectedAddress);
-        }
-      }
-      else if(this.boxZoom == 0.25 && newZoom < 16){
-        this.boxZoom = 0.5;
-        if(this.selectedAddress!=null){
-          this.selectAddress(this.selectedAddress);
-        }
-      }
-      // if(newZoom<)
-      // if()
-      console.log(newZoom);
+      this.map2.setView(center,newZoom)
+      // if(this.boxZoom > 0.25 && newZoom >=16){
+      //   this.boxZoom = 0.25;
+      //   if(this.selectedAddress!=null){
+      //     this.selectAddress(this.selectedAddress);
+      //   }
+      // }
+      // else if(this.boxZoom<0.5 && newZoom < 16){
+      //   this.boxZoom = 0.5;
+      //   if(this.selectedAddress!=null){
+      //     this.selectAddress(this.selectedAddress);
+      //   }
+      // }
+      // console.log(newZoom);
+    });
+
+    this.map2.on('zoomend', (e:any) => {
+      let center = this.map2.getCenter();
+      let newZoom = e.target._zoom;
+      this.map.setView(center,newZoom)
     });
 
     const tiles = leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -277,7 +346,14 @@ export class HomeComponent implements AfterViewInit {
       attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
     });
 
+    const tiles2 = leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      maxZoom: 18,
+      minZoom: 3,
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    });
+
     tiles.addTo(this.map);
+    tiles2.addTo(this.map2);
   }
 
   hslToHex(h: number, s:number, l:number) {
@@ -294,7 +370,7 @@ export class HomeComponent implements AfterViewInit {
 }
 
 interface DisplayOption{
-  value: boolean;
+  value: number;
   display: string;
 }
 
